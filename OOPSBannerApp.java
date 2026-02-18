ea2228629704f07071cb66057ab5ee910cfe3b11
jitheshgokul <jitheshg111@gmail.com>
2026-02-18 23:05:52 +0530

    Complete UC2 ( updated ): Added print OOPS banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,9 +1,11 @@
-public class OOPSBannerApp {
+public class OopsBanner {
     public static void main(String[] args) {
-        System.out.println("  ***    ***   ****   **** "); 
-        System.out.println(" *   *  *   *  *   *  *    "); 
-        System.out.println(" *   *  *   *  ****   **** "); 
-        System.out.println(" *   *  *   *  *          *"); 
-        System.out.println("  ***    ***   *      **** "); 
+        System.out.println("  *****  " + "  *****  " + " *****   " + "  *****  ");
+        System.out.println(" *     * " + " *     * " + " *    *  " + " *       ");
+        System.out.println(" *     * " + " *     * " + " *    *  " + " *       ");
+        System.out.println(" *     * " + " *     * " + " *****   " + "  *****  ");
+        System.out.println(" *     * " + " *     * " + " *       " + "       * ");
+        System.out.println(" *     * " + " *     * " + " *       " + "       * ");
+        System.out.println("  *****  " + "  *****  " + " *       " + "  *****  ");
     }
 }
