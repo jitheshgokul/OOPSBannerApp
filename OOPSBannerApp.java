ffa9367c241d5561ddfc4613c01cdc12156e1762
jitheshgokul <jitheshg111@gmail.com>
2026-02-21 09:30:37 +0530

    Complete UC6 : Added Print Banner using static functions

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,16 +1,47 @@
 public class OOPSBannerApp{
-    public static void main(String[] args){
-        String[] lines = {
-            String.join(" ", "  ***** ", "  ***** ", " *****  ", " *****  "),
-            String.join(" ", " *     *", " *     *", " *    * ", "*       "),
-            String.join(" ", " *     *", " *     *", " *    * ", "*       "),
-            String.join(" ", " *     *", " *     *", " *****  ", " *****  "),
-            String.join(" ", " *     *", " *     *", " *      ", "      * "),
-            String.join(" ", " *     *", " *     *", " *      ", "      * "),
-            String.join(" ", "  ***** ", "  ***** ", " *      ", " *****  ")
+    public static String[] getOPattern(){
+        return new String[]{
+            "  ***** ",
+            " *     *",
+            " *     *",
+            " *     *",
+            " *     *",
+            " *     *",
+            "  ***** "
         };
-        for (String line:lines){
-            System.out.println(line);
+    }
+    public static String[] getPPattern(){
+        return new String[]{
+            " *****  ",
+            " *    * ",
+            " *    * ",
+            " *****  ",
+            " *      ",
+            " *      ",
+            " *      "
+        };
+    }
+    public static String[] getSPattern(){
+        return new String[]{
+            " *****  ",
+            "*       ",
+            "*       ",
+            " *****  ",
+            "      * ",
+            "      * ",
+            " *****  "
+        };
+    }
+    public static void main(String[] args){
+        String[] Opattern = getOPattern();
+        String[] Ppattern = getPPattern();
+        String[] Spattern = getSPattern();
+        for (int i=0;i<Opattern.length;i++){
+            System.out.println(" "+Opattern[i]+" "+Opattern[i]+" "+Ppattern[i]+" "+Spattern[i]);
         }
     }
-};
+
+    
+    
+}
+
